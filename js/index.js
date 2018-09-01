df7fba43de9035771d7a2e589f0d5266d34fc706
wilson Acevedo <wacevedo@everis.com>
2018-09-01 14:56:39 -0500

    se crea la funcion de reset

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@ var autonumero = Math.floor(Math.random()*10 +1); // el ramdom solo devuelve num
 var listaDeNumeros = " ";
 var respuesta =document.querySelector('#numero2')
 var mayorMenor=document.querySelector('#imp_pista');
+ // Puede traer varios objetos
+
 function imprimerNumero(){
-    let ingreso = document.querySelector('#idnumero').value; // Puede traer varios objetos
+    var ingreso = document.querySelector('#idnumero').value;
     
     //document.getElementById("numero2").innerHTML = autonumero; //trae un solo objeto
     listaDeNumeros = listaDeNumeros + " "+ ingreso;
@@ -12,7 +14,7 @@ function imprimerNumero(){
     if(ingreso==autonumero){
         gano();
     }else{
-        pista(autonumero);
+        pista(autonumero,ingreso);
         fallo();
         
     }
@@ -28,12 +30,16 @@ function gano(){
     document.getElementById("numero2").innerHTML = "Gano !"
     respuesta.textContent ="Gano !"
     mayorMenor.textContent=" ";
+    document.querySelector('#boton').setAttribute("disabled", false);
+    document.querySelector('#reset').removeAttribute("disabled");
     //console.log("Adivino ! Felicitaciones ");
 }
 function fallo(){
     if (intentos >= 3){
         console.log("Mejor Suerte para la proxima");
        // document.getElementById("numero2").innerHTML = "Perdio !"
+        document.querySelector('#boton').setAttribute("disabled", false);
+        document.querySelector('#reset').removeAttribute("disabled");
         respuesta.textContent ="Perdio!"
     }else{
         console.log("Vuela a intentarlo");
@@ -43,17 +49,29 @@ function fallo(){
 }
 
 
-function pista( autonumero){
-    if(autonumero>5){
-        mayorMenor.textContent="El numero es mayor que 5";
+function pista( autonumero,ingreso){
+    console.log(ingreso)
+    if(autonumero>ingreso){
+        mayorMenor.textContent="El numero es mayor que "+ ingreso;
         mayorMenor.style.background ="yellow";
         console.log("Es Mayor que 5")
 
     }else{
-        mayorMenor.textContent="El numero es Menor que 5";
+        mayorMenor.textContent="El numero es Menor que "+ ingreso.toString();
         mayorMenor.style.background ="yellow";
         console.log("Es menor que 5")
     }
+}
+
+function reset(){
+    intentos=0;
+    console.log(intentos);
+    let ingreso = document.querySelector('#idnumero').value="";
+    document.querySelector('#reset').setAttribute("disabled", false);
+    document.querySelector('#boton').removeAttribute("disabled");
+    respuesta.textContent =""
+    mayorMenor.textContent=" ";
+
 }
 
     //document.getElementById("idnumero").value= autonumero;
